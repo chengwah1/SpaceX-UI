784eb7bb9a33f19e634d48d71292c04cd9dfde0a
cheng-wah <chengwah1@hotmail.com>
2019-04-17 00:44:13 +0800

    set up primary color for antd

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import DatePicker from 'antd/lib/date-picker';
+import { Button } from 'antd';
 
 // Components
 import LoginFormHeader from './LoginFormHeader';
 
 const Login = ()=>{
     return(
-        <div>
+        <div className="LoginCSS">
             <LoginFormHeader />
             This is login page
             <DatePicker />
+            <Button type="primary">Primary</Button>
         </div>
     )
 }
