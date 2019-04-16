e036188085d2a75efe97609b38124bdbfd722046
cheng-wah <chengwah1@hotmail.com>
2019-04-16 20:20:24 +0800

    setup apollo graphql and confirm working with Test.js

---
+++ src/Component/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// Component
+import Test from './Test'
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Test/>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './Component/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+
+// const httpLink = createHttpLink({
+//     uri: 'https://api.spacex.land/graphql/'
+//   })
+
+const client = new ApolloClient({
+    link: ApolloLink.from([
+        onError(({ graphQLErrors, networkError }) => {
+          if (graphQLErrors)
+            graphQLErrors.map(({ message, locations, path }) =>
+              console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+              ),
+            );
+          if (networkError) console.log(`[Network error]: ${networkError}`);
+        }),
+        new HttpLink({
+          uri: 'https://api.spacex.land/graphql',
+          credentials: 'same-origin'
+        })
+      ]),
+    cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
