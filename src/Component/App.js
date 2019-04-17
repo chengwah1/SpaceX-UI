abf7a9f4e3518f8f0ed0fac37f1bd32fdbe341e6
cheng-wah <chengwah1@hotmail.com>
2019-04-17 08:28:22 +0800

    added 404 page for non specified route

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 // Component
 import Test from './Test';
 import Login from './Login';
+import NotFound from './NotFound';
 import '../styles/App.css';
 
 class App extends Component {
+  state = {
+    username:'',
+    password:'',
+    isLogin:false
+  }
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Route exact path="/" component={Login}/>
-          <Route path="/test" component={Test}/>
+          <Switch>
+            <Route exact path="/" component={Login}/>
+            <Route path="/test" component={Test}/>
+            <Route component={NotFound}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
